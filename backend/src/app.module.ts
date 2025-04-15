09bf83b2182999560997adade6c1b227f6ed595c
D-Ace0 <139914305+D-Ace0@users.noreply.github.com>
2025-04-15 07:38:15 +0200

    Mongodb prepared

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: JwtService,
+  }],
+  imports: [AuthModule,
+    MongooseModule.forRoot('mongodb+srv://atamerfawzy:GIUPM@cluster0.mnyhh2g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+  ],
 })
 export class AppModule {}
