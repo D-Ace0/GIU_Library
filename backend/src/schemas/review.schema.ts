f833d32429af48c19f9f4b2c514e57fce0465c90
AhmedishimAplus <atamerfawzy@gmail.com>
2025-04-22 12:31:27 +0200

    Reviews done

--- backend/src/schemas/review.schema.ts
+++ backend/src/schemas/review.schema.ts
@@ -5,11 +5,12 @@ import {Book} from "./book.schema";
 export type UserDocument = Review & Document;
 @Schema()
 export class Review extends Document {
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: User }) userId: any;
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Book }) bookId: any;
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: () => User}) userId: any;
+    @Prop() BookTitle: string;
+    @Prop() Username: string;
     @Prop() rating: number;
     @Prop() reviewText: string;
-    @Prop() createdAt: Date;
+    @Prop({type:Date}) createdAt: Date;
 }
 
 export const ReviewSchema = SchemaFactory.createForClass(Review);
